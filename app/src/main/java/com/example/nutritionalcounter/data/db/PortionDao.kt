54a76e11901c75a3f4c6e5405e21605ea23ff2aa
Pál Orsolya <pal.orsolya@fornax.hu>
2026-08-16 11:32:20 +0200

    PortionDao CRUD operations created.

--- app/src/main/java/com/example/nutritionalcounter/data/db/PortionDao.kt
+++ app/src/main/java/com/example/nutritionalcounter/data/db/PortionDao.kt
@@ -1,14 +1,32 @@
 package com.example.nutritionalcounter.data.db
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 
 @Dao
 interface PortionDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPortion(portion: Portion)
+
+    @Update
+    suspend fun updatePortion(portion: Portion)
+
+    @Delete
+    suspend fun deletePortion(portion: Portion)
+
     @Transaction
     @Query("select * from portions")
-    fun getPortitionsWithNutrition(): Flow<List<PortionWithNutrition>>
+    fun getPortionsWithNutrition(): Flow<List<PortionWithNutrition>>
+
+    @Transaction
+    @Query("select * from portions where portions.date = :date")
+    fun getPortionsWithNutritionByDate(date: LocalDate): Flow<List<PortionWithNutrition>>
 }
